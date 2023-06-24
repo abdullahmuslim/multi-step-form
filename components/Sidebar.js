952436b973c72a1ddcfcb8e8b42ef34f7d55b3de
muslimabdullah925@gmail.com <muslimabdullah925@gmail.com>
2023-06-24 21:11:10 +0100

    center nav buttons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -22,34 +22,34 @@ function Sidebar({setContent}){
     setContent(components[elID]);
   }
   return (
-    <div className="row">
+    <div className="sidebar row align-items-center justify-content-center py-4">
       {/*<!-- Sidebar start -->*/}
-      <div className="col" onClick={changeMainContent}>
-        <small className="btn btn-outline-secondary rounded-pill px-3 py-2">1</small>
+      <div className="col p-0 m-0" onClick={changeMainContent} onMouseOver={changeMainContent}>
+        <small className="btn btn-outline-secondary rounded-pill d-flex justify-content-center align-items-center">1</small>
         <div className="d-none d-md-inline">
           <h1>Step 1</h1>
           <small>Your info</small>
         </div>
       </div>
       
-      <div className="col" onClick={changeMainContent}>
-        <small className="btn btn-outline-secondary rounded-pill px-3 py-2">2</small>
+      <div className="col p-0 m-0" onClick={changeMainContent} onMouseOver={changeMainContent}>
+        <small className="btn btn-outline-secondary rounded-pill d-flex justify-content-center align-items-center">2</small>
         <div className="d-none d-md-inline">
           <h1>Step 2</h1>
           <small>Select plan</small>
         </div>
       </div>
       
-      <div className="col" onClick={changeMainContent}>
-        <small className="btn btn-outline-secondary rounded-pill px-3 py-2">3</small>
+      <div className="col p-0 m-0" onClick={changeMainContent} onMouseOver={changeMainContent}>
+        <small className="btn btn-outline-secondary rounded-pill d-flex justify-content-center align-items-center">3</small>
         <div className="d-none d-md-inline">
           <h1>Step 3</h1>
           <small>Add - ons</small>
         </div>
       </div>
       
-      <div className="col" onClick={changeMainContent}>
-        <small className="btn btn-outline-secondary rounded-pill px-3 py-2">4</small>
+      <div className="col p-0 m-0" onClick={changeMainContent} onMouseOver={changeMainContent}>
+        <small className="btn btn-outline-secondary rounded-pill d-flex justify-content-center align-items-center">4</small>
         <div className="d-none d-md-inline">
           <h1>Step 4</h1>
           <small>Summary</small>
